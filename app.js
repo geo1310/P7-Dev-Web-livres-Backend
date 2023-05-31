c875ade580cd85d3c7f487bd216fa3f23ea2b580
Georges Briche <gbriche59@yahoo.fr>
2023-05-31 17:35:35 +0200

    Création user

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const axios = require('axios')
 const app = express();
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user')
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue' });
+//connexion a la base de donees
+mongoose
+    .connect(
+        'mongodb+srv://gbriche59:amelie59@cluster0.lmuexid.mongodb.net/?retryWrites=true&w=majority',
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// ajout des headers à la reponse
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    );
+    next();
 });
+
+app.use(express.json())
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const normalizePort = (val) => {
     }
     return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '4000');
 app.set('port', port);
 const errorHandler = (error) => {
     if (error.syscall !== 'listen') {
