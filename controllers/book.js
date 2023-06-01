8f8c81ac5f0f891c7f0253d594ca8b56ed6743ed
Georges Briche <gbriche59@yahoo.fr>
2023-06-01 17:31:14 +0200

    Mise a jour code - tests sur images

--- controllers/book.js
+++ controllers/book.js
@@ -39,14 +39,17 @@ exports.modifyBook = (req, res, next) => {
             if (book.userId != req.auth.userId) {
                 res.status(401).json({ message: 'Not authorized' });
             } else {
-                Book.updateOne(
-                    { _id: req.params.id },
-                    { ...bookObject, _id: req.params.id }
-                )
-                    .then(() =>
-                        res.status(200).json({ message: 'Objet modifié!' })
+                const filename = book.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Book.updateOne(
+                        { _id: req.params.id },
+                        { ...bookObject, _id: req.params.id }
                     )
-                    .catch((error) => res.status(401).json({ error }));
+                        .then(() =>
+                            res.status(200).json({ message: 'Objet modifié!' })
+                        )
+                        .catch((error) => res.status(401).json({ error }));
+                });
             }
         })
         .catch((error) => {
