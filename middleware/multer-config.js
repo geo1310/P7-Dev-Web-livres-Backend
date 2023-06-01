12c5355d879786a5d70524cb6ec1cb40241c7a8b
Georges Briche <gbriche59@yahoo.fr>
2023-06-01 15:24:28 +0200

    Modification multer-config + test code

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -3,7 +3,8 @@ const multer = require('multer');
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
-  'image/png': 'png'
+  'image/png': 'png',
+  'image/webp': 'webp'
 };
 
 const storage = multer.diskStorage({
@@ -11,9 +12,9 @@ const storage = multer.diskStorage({
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
+    const name = file.originalname.split('.')[0];
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    callback(null, name + '_' + Date.now() + '.' + extension);
   }
 });
 module.exports = multer({storage: storage}).single('image');
