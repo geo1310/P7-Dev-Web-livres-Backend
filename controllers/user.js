9f65698c2e483ca4bb2ff57499f6ea3dcfa0bf6f
Georges Briche <gbriche59@yahoo.fr>
2023-06-15 10:06:47 +0200

    Mise en place de variables d'environnement

--- controllers/user.js
+++ controllers/user.js
@@ -32,7 +32,7 @@ exports.signup = (req, res, next) => {
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
+                            process.env.TOKEN_SECRET,
                             { expiresIn: '24h' }
                         )
                     });
--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,4 +1,6 @@
 const multer = require('multer');
+const sharp = require("sharp");
+const fs = require("fs");
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
@@ -19,3 +21,32 @@ const storage = multer.diskStorage({
 });
 
 module.exports = multer({ storage: storage }).single('image');
+
+
+
+
+/*
+
+const multer = require('multer');
+
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+    'image/webp': 'webp',
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'images');
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split('.')[0];
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + '_' + Date.now() + '.' + extension);
+    },
+});
+
+module.exports = multer({ storage: storage }).single('image');
+
+*/
